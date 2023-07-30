06b05445b3d3a18736fc6448cfb10fba3689a818
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-07-30 08:40:58 +0100

    refactor: modify the entire definitions to an object. fix the components to align with the updates

--- part1/courseInfo/src/App.js
+++ part1/courseInfo/src/App.js
@@ -51,41 +51,34 @@ const Total = (props) => {
 }
 
 const App = () => {
-  // const course = "Half Stack application development"
-  // const part1 = "Fundamentals of React"
-  // const exercises1 = 10
-  // const part2 = "Using props to pass data"
-  // const exercises2 = 7
-  // const part3 = "State of a component"
-  // const exercises3 = 14
-
-   const course = "Half Stack application development"
-
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ]
+   const course = {
+     name: "Half Stack application development",
+     parts: [
+       {
+         name: "Fundamentals of React",
+         exercises: 10,
+       },
+       {
+         name: "Using props to pass data",
+         exercises: 7,
+       },
+       {
+         name: "State of a component",
+         exercises: 14,
+       },
+     ],
+   }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
 
       <Content>
-        <Part1 part1={parts[0].name} exercises1={parts[0].exercises} />
-        <Part2 part2={parts[1].name} exercises2={parts[1].exercises} />
-        <Part3 part3={parts[2].name} exercises3={parts[2].exercises} />
+        <Part1 part1={course.parts[0].name} exercises1={course.parts[0].exercises} />
+        <Part2 part2={course.parts[1].name} exercises2={course.parts[1].exercises} />
+        <Part3 part3={course.parts[2].name} exercises3={course.parts[2].exercises} />
       </Content>
-      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Total exercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
