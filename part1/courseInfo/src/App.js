bb31cb75498303c4f11b60f98059284b9d9f8e45
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-07-30 08:34:31 +0100

    refactor: modify all constant definition and other parts needed

--- part1/courseInfo/src/App.js
+++ part1/courseInfo/src/App.js
@@ -51,24 +51,41 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  // const course = "Half Stack application development"
+  // const part1 = "Fundamentals of React"
+  // const exercises1 = 10
+  // const part2 = "Using props to pass data"
+  // const exercises2 = 7
+  // const part3 = "State of a component"
+  // const exercises3 = 14
+
+   const course = "Half Stack application development"
+
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ]
 
   return (
     <div>
       <Header course={course} />
 
       <Content>
-        <Part1 part1={part1} exercises1={exercises1} />
-        <Part2 part2={part2} exercises2={exercises2} />
-        <Part3 part3={part3} exercises3={exercises3} />
+        <Part1 part1={parts[0].name} exercises1={parts[0].exercises} />
+        <Part2 part2={parts[1].name} exercises2={parts[1].exercises} />
+        <Part3 part3={parts[2].name} exercises3={parts[2].exercises} />
       </Content>
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
