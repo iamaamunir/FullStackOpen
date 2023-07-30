19e432a5be0f25155a881302c461c1d55efe5333
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-07-30 05:08:04 +0100

    fix: Content component render to children components

---
+++ part1/courseInfo/src/App.js
@@ -0,0 +1,76 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+
+
+const Content = ({ children }) => {
+  return <div>{children}</div>
+}
+
+const Part1 = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+    </div>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+    </div>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+
+      <Content>
+        <Part1 part1={part1} exercises1={exercises1} />
+        <Part2 part2={part2} exercises2={exercises2} />
+        <Part3 part3={part3} exercises3={exercises3} />
+      </Content>
+      <Total exercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
